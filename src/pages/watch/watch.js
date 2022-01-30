a4288b34f961fc7973b7baa295606e794de12a7c
Ugo Anayo <Ugo.Anayo@sterling.ng>
2022-01-30 17:33:11 +0100

    add api cross origin

--- src/pages/watch/watch.js
+++ src/pages/watch/watch.js
@@ -14,6 +14,7 @@ export default () => {
   };
 
   const socket = io.connect(SERVER, {
+    origin: SERVER,
     'Access-Control-Allow-Origin': SERVER,
   });
   const video = document.querySelector('#watchVideo');

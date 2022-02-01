1f10ad2ac254cad478496b215afe948d37dce581
Holajuwon <henrijay00@gmail.com>
2022-02-01 07:18:44 +0100

    bg(fixes): made little fixes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,30 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import Broadcast from '../pages/broadcast';
 import Watch from '../pages/watch';
 import PageNotFound from '../pages/404page';
 
+const Home = () => (
+  <div>
+    <h1>Welcome to TQL</h1>
+    <p>This is a simple web application to broadcast and watch live streams.</p>
+    <p>
+      You can use this application to broadcast your own live stream or to watch
+      other live streams.
+    </p>
+    <p>
+      You can also use this application to watch live streams from other users.
+    </p>
+    <Link to="/broadcast">Broadcast | </Link>
+    <Link to="/watch">Watch</Link>
+  </div>
+);
 export default class Router extends Component {
   render() {
     return (
       <Routes>
+        <Route exact path="/" element={<Home />} />
         <Route exact path="/broadcast" element={<Broadcast />} />
         <Route exact path="/watch" element={<Watch />} />
         <Route path="*" element={<PageNotFound />} />
